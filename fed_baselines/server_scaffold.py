09387e0adad2dbc551c93d131fe8838eb4efb29a
zpx2022 <134719788+zpx2022@users.noreply.github.com>
2025-07-12 13:52:30 +0800

    Update server_scaffold.py

--- fed_baselines/server_scaffold.py
+++ fed_baselines/server_scaffold.py
@@ -1,83 +1,63 @@
-from utils.fed_utils import init_model
-from fed_baselines.server_base import FedServer
 import copy
-
+import torch
+from fed_baselines.server_base import FedServer
+from utils.fed_utils import init_model
 
 class ScaffoldServer(FedServer):
-    def __init__(self, client_list, dataset_id, model_name):
-        super().__init__(client_list, dataset_id, model_name)
-        # server control variate
+    """Server implementation for the SCAFFOLD algorithm."""
+
+    def __init__(self, client_list, dataset_id, model_name, config):
+        super().__init__(client_list, dataset_id, model_name, config)
+        # Initialize the server control variate (c)
         self.scv = init_model(model_name=self.model_name, num_class=self._num_class, image_channel=self._image_channel)
-        # Dict of all client control variates
-        self.client_ccv_state = {}
+        for param in self.scv.parameters():
+            param.data.zero_()
+        # Dictionary to store client control variate deltas
+        self.client_ccv_deltas = {}
 
     def agg(self):
-        """
-        Server aggregates normalized models from connected clients using SCAFFOLD
-        :return: Updated global model after aggregation, Averaged loss value, Number of the local data points, server control variate
-        """
-        client_num = len(self.selected_clients)
-        if client_num == 0 or self.n_data == 0:
-            return self.model.state_dict(), 0, 0
+        """Aggregates models and updates the server control variate."""
+        if not self.selected_clients or self.n_data == 0:
+            return self.model.state_dict(), 0, {}
 
-        self.scv.to(self._device)
-        self.model.to(self._device)
+        # --- Part 1: Aggregate client models (standard FedAvg) ---
+        avg_loss = 0.0
         model_state = self.model.state_dict()
-        new_scv_state = copy.deepcopy(model_state)
-        avg_loss = 0
-        # print('number of selected clients in Cloud: ' + str(client_num))
-        for i, name in enumerate(self.selected_clients):
-            if name not in self.client_state:
-                continue
-            for key in self.client_state[name]:
+        for key in model_state:
+            model_state[key] = torch.zeros_like(model_state[key])
 
-                if i == 0:
-                    model_state[key] = self.client_state[name][key] * self.client_n_data[name] / self.n_data
-                    new_scv_state[key] = self.client_ccv_state[name][key] * self.client_n_data[name] / self.n_data
-
-                else:
-                    model_state[key] = model_state[key] + self.client_state[name][key] * self.client_n_data[
-                        name] / self.n_data
-                    new_scv_state[key] = new_scv_state[key] + self.client_ccv_state[name][key] * self.client_n_data[
-                        name] / self.n_data
-
-            avg_loss = avg_loss + self.client_loss[name] * self.client_n_data[name] / self.n_data
-
-        scv_state = self.scv.state_dict()
-
+        for name in self.selected_clients:
+            if name in self.client_state:
+                weight = self.client_n_data[name] / self.n_data
+                for key in model_state:
+                    model_state[key] += self.client_state[name][key] * weight
+                avg_loss += self.client_loss[name] * weight
         self.model.load_state_dict(model_state)
-        self.scv.load_state_dict(new_scv_state)
-        self.round = self.round + 1
 
-        return model_state, avg_loss, self.n_data, scv_state
+        # --- Part 2: Aggregate client control variate deltas (delta_c_i) ---
+        total_delta_c = {key: 0.0 for key in self.scv.state_dict().keys()}
+        for name in self.selected_clients:
+            if name in self.client_ccv_deltas:
+                for key in total_delta_c:
+                    total_delta_c[key] += self.client_ccv_deltas[name][key]
 
-    def rec(self, name, state_dict, n_data, loss, ccv_state):
-        """
-        Server receives the local updates from the connected client k.
-        :param name: Name of client k
-        :param state_dict: Model dict from the client k
-        :param n_data: Number of local data points in the client k
-        :param loss: Loss of local training in the client k
-        :param ccv_state: Normalization coefficient
-        """
-        self.n_data = self.n_data + n_data
-        self.client_state[name] = {}
-        self.client_n_data[name] = {}
-        self.client_ccv_state[name] = {}
+        # --- Part 3: Update server control variate (c) ---
+        scv_state = self.scv.state_dict()
+        with torch.no_grad():
+            for key in scv_state:
+                 if len(self.selected_clients) > 0:
+                    scv_state[key] += total_delta_c[key] / len(self.selected_clients)
+        self.scv.load_state_dict(scv_state)
 
-        self.client_state[name].update(state_dict)
-        self.client_n_data[name] = n_data
-        self.client_loss[name] = {}
-        self.client_loss[name] = loss
-        self.client_ccv_state[name].update(ccv_state)
+        self.round += 1
+        return self.model.state_dict(), avg_loss, self.scv.state_dict()
 
+    def rec(self, name, state_dict, n_data, loss, ccv_delta):
+        """SCAFFOLD-specific receive method for the control variate delta."""
+        super().rec(name, state_dict, n_data, loss)
+        self.client_ccv_deltas[name] = ccv_delta
 
     def flush(self):
-        """
-        Flushing the client information in the server
-        """
-        self.n_data = 0
-        self.client_state = {}
-        self.client_n_data = {}
-        self.client_loss = {}
-        self.client_ccv_state = {}
+        """Clears SCAFFOLD-specific client info for the next round."""
+        super().flush()
+        self.client_ccv_deltas = {}
