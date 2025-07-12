1ff7b5c56ecfadc73d5ff17db8d8c106a1b14576
zpx2022 <134719788+zpx2022@users.noreply.github.com>
2025-07-12 13:52:18 +0800

    Update server_fednova.py

--- fed_baselines/server_fednova.py
+++ fed_baselines/server_fednova.py
@@ -1,79 +1,59 @@
-from fed_baselines.server_base import FedServer
 import copy
-
+from fed_baselines.server_base import FedServer
 
 class FedNovaServer(FedServer):
-    def __init__(self, client_list, dataset_id, model_name):
-        super().__init__(client_list, dataset_id, model_name)
-        # Normalized coefficient
+    """Server implementation for the FedNova algorithm."""
+    
+    def __init__(self, client_list, dataset_id, model_name, config):
+        super().__init__(client_list, dataset_id, model_name, config)
+        # Dictionaries to store FedNova specific values
         self.client_coeff = {}
-        # Normalized gradients
         self.client_norm_grad = {}
 
     def agg(self):
-        """
-        Server aggregates normalized models from connected clients using FedNova
-        :return: Updated global model after aggregation, Averaged loss value, Number of the local data points
-        """
-        client_num = len(self.selected_clients)
-        if client_num == 0 or self.n_data == 0:
-            return self.model.state_dict(), 0, 0
+        """Aggregates models from clients using FedNova logic."""
+        if not self.selected_clients or self.n_data == 0:
+            return self.model.state_dict(), 0, {}
 
         self.model.to(self._device)
-
         model_state = self.model.state_dict()
-        nova_model_state = copy.deepcopy(model_state)
-        avg_loss = 0
-        coeff = 0.0
-        for i, name in enumerate(self.selected_clients):
-            coeff = coeff + self.client_coeff[name] * self.client_n_data[name] / self.n_data
-            for key in self.client_state[name]:
-                if i == 0:
-                    nova_model_state[key] = self.client_norm_grad[name][key] * self.client_n_data[name] / self.n_data
-                else:
-                    nova_model_state[key] = nova_model_state[key] + self.client_norm_grad[name][key] * self.client_n_data[name] / self.n_data
-            avg_loss = avg_loss + self.client_loss[name] * self.client_n_data[name] / self.n_data
 
-        for key in model_state:
-            model_state[key] -= coeff * nova_model_state[key]
+        # Aggregate normalized gradients from clients
+        total_norm_grad = {key: 0.0 for key in model_state.keys()}
+        total_weight = 0.0
+        avg_loss = 0.0
+        
+        for name in self.selected_clients:
+            if name in self.client_state:
+                data_weight = self.client_n_data[name]
+                total_weight += self.client_coeff[name] * data_weight
+                for key in total_norm_grad:
+                    total_norm_grad[key] += self.client_norm_grad[name][key] * data_weight
+                avg_loss += self.client_loss[name] * data_weight
+
+        # Calculate effective local steps
+        effective_local_steps = total_weight / self.n_data if self.n_data != 0 else 0
+        
+        # Update the global model using the aggregated normalized gradients
+        with torch.no_grad():
+            for key in model_state:
+                if self.n_data != 0:
+                    model_state[key] -= effective_local_steps * (total_norm_grad[key] / self.n_data)
 
         self.model.load_state_dict(model_state)
+        avg_loss = avg_loss / self.n_data if self.n_data != 0 else 0
 
-        self.round = self.round + 1
-
-        return model_state, avg_loss, self.n_data
+        self.round += 1
+        return model_state, avg_loss, {}
 
     def rec(self, name, state_dict, n_data, loss, coeff, norm_grad):
-        """
-        Server receives the local updates from the connected client k.
-        :param name: Name of client k
-        :param state_dict: Model dict from the client k
-        :param n_data: Number of local data points in the client k
-        :param loss: Loss of local training in the client k
-        :param coeff: Normalization coefficient
-        :param norm_grad: Normalized gradients
-        """
-        self.n_data = self.n_data + n_data
-        self.client_state[name] = {}
-        self.client_n_data[name] = {}
-        self.client_coeff[name] = -1
-        self.client_norm_grad[name] = {}
-
-        self.client_state[name].update(state_dict)
-        self.client_n_data[name] = n_data
-        self.client_loss[name] = {}
-        self.client_loss[name] = loss
+        """FedNova-specific receive method for extra parameters."""
+        super().rec(name, state_dict, n_data, loss)
         self.client_coeff[name] = coeff
-        self.client_norm_grad[name].update(norm_grad)
-
+        self.client_norm_grad[name] = norm_grad
 
     def flush(self):
-        """
-        Flushing the client information in the server
-        """
-        self.n_data = 0
-        self.client_state = {}
-        self.client_n_data = {}
-        self.client_loss = {}
+        """Clears FedNova-specific client info for the next round."""
+        super().flush()
         self.client_coeff = {}
         self.client_norm_grad = {}
