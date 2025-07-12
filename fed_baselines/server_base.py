a43a91a6416d12728f281d45465a06a0520049cd
zpx2022 <134719788+zpx2022@users.noreply.github.com>
2025-07-12 13:52:01 +0800

    Update server_base.py

--- fed_baselines/server_base.py
+++ fed_baselines/server_base.py
@@ -1,147 +1,103 @@
-from utils.models import *
 import torch
+import torch.nn as nn
+import numpy as np
 from torch.utils.data import DataLoader
+from utils.models import *
 from utils.fed_utils import assign_dataset, init_model
 
-
 class FedServer(object):
-    def __init__(self, client_list, dataset_id, model_name):
-        """
-        Initialize the server for federated learning.
-        :param client_list: List of the connected clients in networks
-        :param dataset_id: Dataset name for the application scenario
-        :param model_name: Machine learning model name for the application scenario
-        """
-        # Initialize the dict and list for system settings
+    """
+    The base class for a federated learning server.
+    It handles model testing, client selection, and standard FedAvg aggregation.
+    """
+    def __init__(self, client_list, dataset_id, model_name, config):
+        # Dictionaries to store updates from clients
         self.client_state = {}
         self.client_loss = {}
         self.client_n_data = {}
         self.selected_clients = []
-        # batch size for testing
-        self._batch_size = 200
+        
+        self._batch_size = config['batch_size']
         self.client_list = client_list
-
-        # Initialize the test dataset
         self.testset = None
-
-        # Initialize the hyperparameter for federated learning in the server
         self.round = 0
         self.n_data = 0
         self._dataset_id = dataset_id
 
-        # Testing on GPU
+        # Set device for testing
         gpu = 0
-        self._device = torch.device("cuda:{}".format(gpu) if torch.cuda.is_available() and gpu != -1 else "cpu")
+        self._device = torch.device(f"cuda:{gpu}" if torch.cuda.is_available() and gpu != -1 else "cpu")
 
-        # Initialize the global machine learning model
+        # Initialize the global model
         self._num_class, self._image_dim, self._image_channel = assign_dataset(dataset_id)
         self.model_name = model_name
         self.model = init_model(model_name=self.model_name, num_class=self._num_class, image_channel=self._image_channel)
 
     def load_testset(self, testset):
-        """
-        Server loads the test dataset.
-        :param data: Dataset for testing.
-        """
+        """Server loads the global test dataset."""
         self.testset = testset
 
     def state_dict(self):
-        """
-        Server returns global model dict.
-        :return: Global model dict
-        """
+        """Server returns the current global model state."""
         return self.model.state_dict()
 
     def test(self):
-        """
-        Server tests the model on test dataset.
-        """
-        test_loader = DataLoader(self.testset, batch_size=self._batch_size, shuffle=True)
+        """Server tests the global model on the test dataset."""
+        test_loader = DataLoader(self.testset, batch_size=self._batch_size, shuffle=False)
         self.model.to(self._device)
-        accuracy_collector = 0
-        for step, (x, y) in enumerate(test_loader):
-            with torch.no_grad():
-                b_x = x.to(self._device)  # Tensor on GPU
-                b_y = y.to(self._device)  # Tensor on GPU
-
-                test_output = self.model(b_x)
-                pred_y = torch.max(test_output, 1)[1].to(self._device).data.squeeze()
-                accuracy_collector = accuracy_collector + sum(pred_y == b_y)
-        accuracy = accuracy_collector / len(self.testset)
-
-        return accuracy.cpu().numpy()
+        self.model.eval()
+        
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data, targets in test_loader:
+                data, targets = data.to(self._device), targets.to(self._device)
+                outputs = self.model(data)
+                _, predicted = torch.max(outputs.data, 1)
+                total += targets.size(0)
+                correct += (predicted == targets).sum().item()
+        
+        accuracy = correct / total
+        return accuracy
 
     def select_clients(self, connection_ratio=1):
-        """
-        Server selects a fraction of clients.
-        :param connection_ratio: connection ratio in the clients
-        """
-        # select a fraction of clients
-        self.selected_clients = []
-        self.n_data = 0
-        for client_id in self.client_list:
-            b = np.random.binomial(np.ones(1).astype(int), connection_ratio)
-            if b:
-                self.selected_clients.append(client_id)
-                self.n_data += self.client_n_data[client_id]
+        """Selects all clients for simplicity in this implementation."""
+        self.selected_clients = self.client_list
+        self.n_data = sum(self.client_n_data.get(client_id, 0) for client_id in self.client_list)
 
     def agg(self):
-        """
-        Server aggregates models from connected clients.
-        :return: model_state: Updated global model after aggregation
-        :return: avg_loss: Averaged loss value
-        :return: n_data: Number of the local data points
-        """
+        """Aggregates models from clients using the FedAvg algorithm."""
         client_num = len(self.selected_clients)
+        self.n_data = sum(self.client_n_data.values())
+
         if client_num == 0 or self.n_data == 0:
             return self.model.state_dict(), 0, 0
 
-        # Initialize a model for aggregation
-        model = init_model(model_name=self.model_name, num_class=self._num_class, image_channel=self._image_channel)
-        model_state = model.state_dict()
-        avg_loss = 0
+        # Initialize a new model state for aggregation
+        model_state = self.model.state_dict()
+        for key in model_state:
+            model_state[key] = torch.zeros_like(model_state[key])
 
-        # Aggregate the local updated models from selected clients
-        for i, name in enumerate(self.selected_clients):
-            if name not in self.client_state:
-                continue
-            for key in self.client_state[name]:
-                if i == 0:
-                    model_state[key] = self.client_state[name][key] * self.client_n_data[name] / self.n_data
-                else:
-                    model_state[key] = model_state[key] + self.client_state[name][key] * self.client_n_data[
-                        name] / self.n_data
-
-            avg_loss = avg_loss + self.client_loss[name] * self.client_n_data[name] / self.n_data
-
-        # Server load the aggregated model as the global model
+        avg_loss = 0.0
+        for name in self.selected_clients:
+            if name in self.client_state:
+                weight = self.client_n_data[name] / self.n_data
+                for key in self.client_state[name]:
+                    model_state[key] += self.client_state[name][key] * weight
+                avg_loss += self.client_loss[name] * weight
+        
         self.model.load_state_dict(model_state)
-        self.round = self.round + 1
-        n_data = self.n_data
-
-        return model_state, avg_loss, n_data
+        self.round += 1
+        return model_state, avg_loss, self.n_data
 
     def rec(self, name, state_dict, n_data, loss):
-        """
-        Server receives the local updates from the connected client k.
-        :param name: Name of client k
-        :param state_dict: Model dict from the client k
-        :param n_data: Number of local data points in the client k
-        :param loss: Loss of local training in the client k
-        """
-        self.n_data = self.n_data + n_data
-        self.client_state[name] = {}
-        self.client_n_data[name] = {}
-
-        self.client_state[name].update(state_dict)
+        """Server receives local updates from a client."""
+        self.client_state[name] = state_dict
         self.client_n_data[name] = n_data
-        self.client_loss[name] = {}
         self.client_loss[name] = loss
 
     def flush(self):
-        """
-        Flushing the client information in the server
-        """
+        """Clears client information for the next round."""
         self.n_data = 0
         self.client_state = {}
         self.client_n_data = {}
